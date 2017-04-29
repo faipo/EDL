72491969729bc1513ef15ac7ce1d01677495aa64
Rafael Teixeira <rafael_teixeira@bitbucket.org>
2017-04-29 19:17:03 -0300

    Tarefa 03

---
+++ trabalhos/tarefa-03/main.lua
@@ -0,0 +1,15 @@
+function love.load(  )
+	
+end
+
+function love.keypressed( key )
+
+end
+
+function love.update( dt )
+	
+end
+
+function love.draw(  )
+	love.graphics.print("Texto na tela",200,250)
+end
